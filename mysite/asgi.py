07985a64cf75a95965687f22dd2d0333967e8c72
Ahmed Brhili <brhiliahmed@gmail.com>
2021-03-18 03:23:29 +0100

    learning BDD and migrations some stuff

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -2,6 +2,7 @@
 ASGI config for mysite project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
+It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
